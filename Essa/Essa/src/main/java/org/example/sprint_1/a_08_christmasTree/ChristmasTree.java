51190f1356da5b5c6f1ee29aac16292b3a59f118
RyszardEnv <119497568+RyszardEnv@users.noreply.github.com>
2023-03-02 11:25:41 +0100

    Update ChristmasTree.java

--- Essa/Essa/src/main/java/org/example/sprint_1/a_08_christmasTree/ChristmasTree.java
+++ Essa/Essa/src/main/java/org/example/sprint_1/a_08_christmasTree/ChristmasTree.java
@@ -1,22 +1,14 @@
 package org.example.sprint_1.a_08_christmasTree;
 
 public class ChristmasTree {
-
-    void printChristmasTree(int n) {
-        for (int i = 1; i <= n; i++) {
-            for (int j = n; j > i; j--) {
-                System.out.println(" ");
-            }
-
-            for (int k = 1; k <= i; k++) {
-                System.out.print("*");
-            }
-
-            for (int l = n; l <= 1; l++) {
-                for (int h = 1; h <= n; h++) {
-                    System.out.print(" ");
-                }
-            }
-        }
-    }
+  
+    public static void printChristmasTree(int n) {
+       for (int i = 0; i < n; i++) {
+           for (int j = 0; j < n - i; j++)
+               System.out.print(" ");
+           for (int k = 0; k < (2 * i + 1); k++)
+               System.out.print("*");
+           System.out.println();
+       }
+   }
 }
